6f4a79c664398e57c1cb8754e8aadb763b9c2b72
Deadeye249 <65706696+deadeye249@users.noreply.github.com>
2025-09-14 21:10:53 +0500

    Revert "Add Logic for Appending Boxes to Container"
    This reverts commit 6997672534bcc740ffa1b7a98f0517f1cbef711b.

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 function createDivs(rows,columns)
-{   
-    let total_boxes = rows*columns;
+{
     let container_div = document.querySelector(".container");
-    for(let i=0;i<total_boxes;i++)
+    for(let i=0;i<rows;i++)
     {
-        let new_box = document.createElement("div");
-        new_box.className = "boxes";
-        new_box.style.cssText= `width: ${100/columns}%; padding-bottom: ${100/columns}%;`
-        container_div.appendChild(new_box);
+        let new_row = document.createElement("div");
+        new_row.className = `grid-row pos${i+1}`;
+        for(let j=0;j<columns;j++)
+        {
+            let new_column = document.createElement("div");
+            new_column.className = `grid-column pos${j+1}`;
+            new_row.appendChild(new_column);
+        }
+        container_div.appendChild(new_row);
     }
 }
 
-createDivs(5,5);
+createDivs(16,16);
